0ef18c329f561de73552648b9f694b53e4f4918d
ansh7mehta <ansh7mehta@gmail.com>
2016-09-18 16:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile('Article 1 is requested and it will be served.');
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile('Article 2 is requested and it will be served.');
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile('Article 3 is requested and it will be served.');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
